69ac3a646f463935e92008f6afd6626cb84a2c59
Dmitry Rymar <goehoture5454@gmail.com>
2022-10-21 21:55:16 +0500

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Post post = new Post();
+
+        post.name = "Дмитрий";
+        post.passport = "5566 & 777888";
+        post.patronymic = "Петрович";
+        post.phone = "8_900_000_33_22";
+        post.surname = "Иванов";
+        post.subscription = true;
+        post.birthday.day = 13;
+        post.birthday.month = 11;
+        post.birthday.year = 2000;
+
+    }
+}
